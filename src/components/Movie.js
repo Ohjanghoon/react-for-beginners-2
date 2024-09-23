2b34023cc20cab38394419d28c9655a8be5e9385
Ohjanghoon <ojh4105@naver.com>
2024-09-23 16:44:06 +0900

    7.5 React Router

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,22 @@
 import PropTypes from "prop-types";
+import {Link} from "react-router-dom";
 
 function Movie({coverImg, title, summary, genres}){
     return (
-        <div>
-              <img src={coverImg} alt={title}/>
-              <h2>{title}</h2>
-              <p>{summary}</p>
-              {movie.hasOwnProperty("genres") ? (<ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>) : null}
-            </div>
+      <div>
+        <img src={coverImg} alt={title}/>
+        <h2>
+          <Link to="/movie">
+          {title}
+          </Link>
+        </h2>
+        <p>{summary}</p>
+        {genres !== undefined ? (<ul>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>) : null}
+      </div>
     );
 }
 
